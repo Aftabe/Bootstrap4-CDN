898834254022f88c175ebef46d23b26cf72bc665
Sunny <47844914+Aftabe@users.noreply.github.com>
2020-03-06 21:11:50 +1100

    code added again

---
+++ script.js
@@ -0,0 +1,5 @@
+console.log('This is the text where you can see in the console, statement one');
+console.log('This is the text where you can see in the console, statement two');
+
+var name = prompt('What is your name budy!');
+document.write( "Welcome" + name);
