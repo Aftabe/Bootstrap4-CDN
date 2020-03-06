294fcdfc72b5d9cde3770171e48937b9eb7d505f
Sunny <47844914+Aftabe@users.noreply.github.com>
2020-03-06 21:10:12 +1100

    code removed

--- script.js
+++
@@ -1,5 +0,0 @@
-console.log('This is the text where you can see in the console, statement one');
-console.log('This is the text where you can see in the console, statement two');
-
-var name = prompt('What is your name budy!');
-document.write( "Welcome" + name);
