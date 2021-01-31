274b84ad6ccf114724538b1d7d641aab20c3c154
Aftabe <shamsasghari77@gmail.com>
2021-01-31 21:08:53 +1100

    Some js code added

--- script.js
+++ script.js
@@ -2,9 +2,11 @@ var el = document.getElementById('btn');
 
 el.addEventListener('click', saySomeThing);
 
-
 function saySomeThing(){
     // document.body.style.backgroundColor = 'blue';
     var cls = this.innerHTML.toLowerCase();
     document.body.className = cls;
 }
+document.body.addEventListener('click', function(){
+    document.body.innerHTML = ' ';
+})
