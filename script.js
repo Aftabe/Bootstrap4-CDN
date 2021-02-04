5451baf4d990aabf27fff2d2240887b95d9fb597
Aftabe <shamsasghari77@gmail.com>
2021-02-04 21:35:13 +1100

    Js code block added

--- script.js
+++ script.js
@@ -18,3 +18,26 @@ btn.style.padding = '10px 40px';
 btn.style.borderRadius = '10px';
 // btn.style.border = 'none';
 btn.style.borderColor = '#4BB977';
+
+
+// How to create an object in js
+const client = {
+    person: 'Juan',
+    balance: 2000,
+    membership : function() {
+           let name;
+           // Check different Balance
+           if(this.balance > 1000) {
+              name = 'Gold';
+           } else if(this.saldo > 500) {
+              name = 'Platinum';
+           } else {
+              name  = 'Normal';
+           }
+           return name;
+    }
+ }
+ console.log(client);
+//  console.log(client.name);
+//  console.log(client.balance);
+//  console.log(client.membership() );
